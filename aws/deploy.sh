c9bd84881f49c137cfc66604ec6c32d35664da9a
vladimir.khazin <vladimir.khazin@icssolutions.ca>
2017-09-10 12:36:17 -0400

    remove teraform binary

--- aws/deploy.sh
+++ aws/deploy.sh
@@ -1,13 +1,15 @@
 clear
+sudo apt install -y zip
 
 pwd=$(pwd)
 cd ./aws/terraform
 
-sudo apt install -y zip
+curl -O https://releases.hashicorp.com/terraform/0.10.4/terraform_0.10.4_linux_amd64.zip
+unzip ./terraform_0.10.4_linux_amd64.zip
 
 zip ./deployment.zip -r ./ -x *.git* -x *aws* -x terraform*
-./bin/terraform init ./ -yes
-./bin/terraform apply ./
+./terraform init ./ -yes
+./terraform apply ./
 
 rm ./deployment.zip
 
